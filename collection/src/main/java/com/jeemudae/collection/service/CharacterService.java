cba64f1328bd166b8f7f89b76802b80c50182a35
ThomasGirotti <standage_thediet@8shield.net>
2024-11-29 18:21:19 +0100

    OPQIUHFGOPQSfqsq

--- collection/src/main/java/com/jeemudae/collection/service/CharacterService.java
+++ collection/src/main/java/com/jeemudae/collection/service/CharacterService.java
@@ -2,44 +2,6 @@ package com.jeemudae.collection.service;
 
 import java.util.List;
 
-import org.springframework.context.ApplicationEventPublisher;
-import org.springframework.stereotype.Service;
-
-import com.jeemudae.collection.repository.Character;
-import com.jeemudae.collection.repository.CharacterRepository;
-import com.jeemudae.collection.repository.CollectionSet;
-import com.jeemudae.collection.repository.CollectionSetRepository;
-import com.jeemudae.collection.repository.User;
-
-import jakarta.transaction.Transactional;
-
-@Service
-public class CharacterService {
-    private final CollectionSetRepository collectionSetRepository;
-    private final CharacterRepository characterRepository;
-    private final ApplicationEventPublisher eventPublisher;
-
-    public CharacterService(CharacterRepository characterRepository, ApplicationEventPublisher eventPublisher, CollectionSetRepository collectionSetRepository) {
-        this.collectionSetRepository = collectionSetRepository;
-        this.characterRepository = characterRepository;
-        this.eventPublisher = eventPublisher;
-    }
-
-    public List<Character> getAllCharacters() {
-        return characterRepository.findAll();
-    }
-
-    public List<Character> getCharactersForUser(User user) {
-        CollectionSet collectionSet = user.getCollectionSet();
-        return characterRepository.findByCollectionSet(collectionSet);
-    }
-
-    public Character getCharacterById(Long characterId) {
-        return characterRepository.findById(characterId).orElse(null);
-    }
-
-    @Transactional
-    public void saveCharacter(Character character) {
         characterRepository.save(character);
         //eventPublisher.publishEvent(new CharacterClaimedEvent(character)); //TODO: Event to maintain collection value across all users
     }
--- collection/src/main/java/com/jeemudae/collection/service/RollService.java
+++ collection/src/main/java/com/jeemudae/collection/service/RollService.java
@@ -5,35 +5,4 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
-import com.jeemudae.collection.repository.Character;
-import com.jeemudae.collection.repository.CollectionSet;
-import com.jeemudae.collection.repository.CollectionSetRepository;
-import com.jeemudae.collection.repository.User;
-
-import jakarta.transaction.Transactional;
-
-@Service
-public class RollService {
-    private final CharacterService characterService;
-    private final CollectionSetRepository collectionSetRepository;
-
-    public RollService(CharacterService characterService, CollectionSetRepository collectionSetRepository) {
-        this.characterService = characterService;
-        this.collectionSetRepository = collectionSetRepository;
-    }
-
-    @Transactional
-    public List<Character> rollCharacters() {
-        List<Character> allCharacters = characterService.getAllCharacters();
-        Collections.shuffle(allCharacters);
-        return allCharacters.subList(0, Math.min(10, allCharacters.size()));
-    }
-
-    @Transactional
-    public void claimCharacter(User user, Character character) {
-        CollectionSet collectionSet = user.getCollectionSet();
-        collectionSet.addCharacter(character);
-        collectionSetRepository.save(collectionSet);
-        characterService.saveCharacter(character);
-    }
-}
+import com.jeemudae.collection.repository.Charac
