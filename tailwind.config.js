a8011dacaf308fb20b54f1da0a63a33d82c949a4
Smooss <smoss.bgt@gmail.com>
2024-11-29 21:17:11 +0100

    tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './src/**/*.html',
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
+
 
