ac519991ff3858621437effb60de5e6c78e8b9a6
Sulfy6768 <liliandel.etoile@gmail.com>
2024-11-30 13:39:54 +0100

    Add Top Users feature with ranking by collection value

--- collection/src/main/java/com/jeemudae/collection/repository/UserRepository.java
+++ collection/src/main/java/com/jeemudae/collection/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package com.jeemudae.collection.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -12,4 +13,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
     @Query("SELECT u FROM User u LEFT JOIN FETCH u.following WHERE u.username = :username")
     Optional<User> findByUsernameWithFollowing(@Param("username") String username);
+    @Query("SELECT u FROM User u JOIN u.collectionSet c JOIN c.characters ch GROUP BY u ORDER BY SUM(ch.price) DESC")
+    List<User> findTopUsersByCollectionValue();
+
 }
