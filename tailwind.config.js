868f638d54fa6bc5b8144fce1c9610e83d6c94e5
Smooss <smoss.bgt@gmail.com>
2024-11-30 15:56:39 +0100

    tailwind test on collection.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/**/*.html',
+    './src/main/resources/templates/**/*.html', // Inclut tous les fichiers HTML Thymeleaf
+    './src/main/resources/static/**/*.html',    // Inclut tous les fichiers HTML statiques
+    './src/main/resources/static/**/*.js',      // Inclut les scripts JS si nécessaire
   ],
   theme: {
     extend: {},
   },
   plugins: [],
 };
-
-
